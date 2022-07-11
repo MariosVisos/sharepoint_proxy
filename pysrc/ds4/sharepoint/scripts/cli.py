07f704e19a1c32db46c208518edd5adb2c4e374f
marios_visos <visos.marios@gmail.com>
2022-07-11 17:53:26 +0200

    Add command for getting all sites

--- pysrc/ds4/sharepoint/scripts/cli.py
+++ pysrc/ds4/sharepoint/scripts/cli.py
@@ -1,7 +1,7 @@
 import click
 from ds4.sharepoint.scripts.auth import oauth
 from ds4.sharepoint.scripts.folders import create_folder, read_folders
-from ds4.sharepoint.scripts.sites import search_site
+from ds4.sharepoint.scripts.sites import get_all_sites, search_site
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
@@ -15,3 +15,4 @@ def cli():
 # cli.add_command(read_folders)
 cli.add_command(oauth)
 cli.add_command(search_site)
+cli.add_command(get_all_sites)
--- pysrc/ds4/sharepoint/scripts/sites.py
+++ pysrc/ds4/sharepoint/scripts/sites.py
@@ -11,3 +11,13 @@ def search_site(keyword=""):
     click.echo("Search SharePoint sites")
     result = search_sites(keyword)
     click.echo(f"get_sites. Result {result}")
+
+
+@click.command()
+def get_all_sites():
+    """Get all SharePoint sites"""
+    # At the moment search is used to get all sites
+    config.init()
+    click.echo("Get all SharePoint sites")
+    result = search_sites()
+    click.echo(f"get_sites. Result {result}")
