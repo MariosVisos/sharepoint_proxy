be210df0fbe29b2b2dcb46032577e12b183355f9
Marios Visos <32926115+MariosVisos@users.noreply.github.com>
2022-07-13 18:01:24 +0200

    Upload a file to a folder

--- pysrc/ds4/sharepoint/scripts/cli.py
+++ pysrc/ds4/sharepoint/scripts/cli.py
@@ -5,7 +5,11 @@ from ds4.sharepoint.scripts.document_libraries import (
     get_document_library,
     get_document_library_items,
 )
-from ds4.sharepoint.scripts.drive_items import get_item, get_items_in_folder
+from ds4.sharepoint.scripts.drive_items import (
+    get_item,
+    get_items_in_folder,
+    upload_file,
+)
 from ds4.sharepoint.scripts.folders import create_folder, read_folders
 from ds4.sharepoint.scripts.sites import get_all_sites, get_site, search_site
 
@@ -28,3 +32,4 @@ cli.add_command(get_document_library)
 cli.add_command(get_document_library_items)
 cli.add_command(get_item)
 cli.add_command(get_items_in_folder)
+cli.add_command(upload_file)
