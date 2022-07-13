74932419280418e7c704fff48d8f48a3016d2743
Marios Visos <32926115+MariosVisos@users.noreply.github.com>
2022-07-13 15:13:43 +0200

    Get single drive item by id

--- pysrc/ds4/sharepoint/scripts/cli.py
+++ pysrc/ds4/sharepoint/scripts/cli.py
@@ -5,6 +5,7 @@ from ds4.sharepoint.scripts.document_libraries import (
     get_document_library,
     get_document_library_items,
 )
+from ds4.sharepoint.scripts.drive_items import get_item
 from ds4.sharepoint.scripts.folders import create_folder, read_folders
 from ds4.sharepoint.scripts.sites import get_all_sites, get_site, search_site
 
@@ -25,3 +26,4 @@ cli.add_command(get_site)
 cli.add_command(get_document_libraries)
 cli.add_command(get_document_library)
 cli.add_command(get_document_library_items)
+cli.add_command(get_item)
